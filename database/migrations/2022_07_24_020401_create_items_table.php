75dd5b13d3baf9bbd916fa0e6a0eb2e8a00b1d76
Thiago Gazaroli Cardoso <Tgazaroli@gmail.com>
2022-07-24 17:58:13 -0300

    add migration

---
+++ database/migrations/2022_07_24_020401_create_items_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use    Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('tombamento')->unique();
+            $table->string('denominacao');
+            $table->string('termo');
+            $table->float('valor');
+            $table->string('tomb_antigo');
+            $table->string('estado');
+            $table->string('situacao');
+            $table->string('n_serie');
+            $table->string('obsercao');
+            $table->foreignId('status_id');
+
+            $table->foreign('status_id')->references('id')->on('status_items');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+};
