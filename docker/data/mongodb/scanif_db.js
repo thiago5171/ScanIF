867d004d6d1264c523af314c3b58b88197a8b7cb
thiago5171 <tgazaroli@gmail.com>
2022-07-30 18:59:39 -0300

    update credential mongo

--- docker/data/mongodb/scanif_db.js
+++ docker/data/mongodb/scanif_db.js
@@ -4,7 +4,7 @@ db.createUser({
 	roles: [
 		{
 			role: "readWrite",
-			db: "post_db",
+			db: "scanif_db",
 		},
 	],
 });
--- docker/data/postgres/appointment_db.sql
+++
@@ -1,5 +0,0 @@
-create database appointment_db;
-
-\c appointment_db;
-
-create extension if not exists "uuid-ossp";
